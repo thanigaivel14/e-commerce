20a3ca5198ebcd75949e3fd2f18e240a46c1b159
thanigai14 <thanigai905@gmail.com>
2026-05-06 11:03:21 +0530

    axios

--- Front_end/src/axios/axios.js
+++ Front_end/src/axios/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const isDev = import.meta.env.VITE_NODE_ENV === "development";
-const url = import.meta.env.DEV
+const url = isDev
   ? "http://localhost:8000/api/v1"
   : import.meta.env.VITE_API_URL;
 const API =axios.create({
